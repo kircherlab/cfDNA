25c566506ed9a6c259456201ce87ba954e1786d9
sroener <40714954+sroener@users.noreply.github.com>
2021-04-15 14:42:28 +0200

    handling IDs in table

--- workflow/scripts/expression_analysis/normalize.py
+++ workflow/scripts/expression_analysis/normalize.py
@@ -12,6 +12,7 @@ background_COV = snakemake.input["background_COV"]
 output_WPS = snakemake.output["output_WPS"]
 output_COV = snakemake.output["output_COV"]
 
+index_choice=None
 
 def normalize_sample(path_a: str, path_b: str):
     """Reads .csv file, calculates mean over all rows and divides by trimmed mean.
@@ -23,17 +24,28 @@ def normalize_sample(path_a: str, path_b: str):
         [type]: [description]
     """
     sample_a = pd.read_csv(path_a, header=None)
+    if pd.api.types.is_string_dtype(sample_a[0]):
+        sample_a = sample_a.set_index(0)
+        sample_a.index.name="ID"
     sample_b = pd.read_csv(path_b, header=None).mean(axis=1)
     sample = sample_a / stats.trim_mean(sample_b, 0.1)
     return sample
 
 normalized_WPS = normalize_sample(path_a=target_WPS, path_b=background_WPS)
 
-normalized_Cov = normalize_sample(path_a=target_COV, path_b=background_COV)
+normalized_COV = normalize_sample(path_a=target_COV, path_b=background_COV)
 
-normalized_WPS.to_csv(output_WPS, sep="\t",header=None, index=None)
+if normalized_WPS.index.is_object():
+    normalized_WPS.to_csv(output_WPS, sep="\t",header=None)
+else:
+    normalized_WPS.to_csv(output_WPS, sep="\t",header=None, index=None)
 
-normalized_Cov.to_csv(output_COV, sep="\t",header=None, index=None)
+if normalized_COV.index.is_object():
+    normalized_COV.to_csv(output_COV, sep="\t",header=None)
+else:
+    normalized_COV.to_csv(output_COV, sep="\t",header=None, index=None)
+
+
 
 
 
