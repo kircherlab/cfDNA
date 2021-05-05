25f288610d85c661ee331df020526f9ea4cec0ab
sroener <40714954+sroener@users.noreply.github.com>
2021-05-05 12:48:58 +0200

    changes because of conflicts

--- workflow/scripts/expression_analysis/snakemake_correlation_table.R
+++ workflow/scripts/expression_analysis/snakemake_correlation_table.R
@@ -1,9 +1,9 @@
 
-samples <- c(snakemake@params[["samples"]])
-ID <- snakemake@params[["ID"]]
+samples <- c(snakemake@input[["samples"]])
+IDs <- snakemake@params[["IDs"]]
 
 print(samples)
-print(ID)
+print(IDs)
 
 # snakemake param für cell-line (für allFreq)-> möglicherweise für ALLE cell-lines?
 ##################################
@@ -24,15 +24,16 @@ dim(logndata)
 tLabels <- read.table(snakemake@input[["labels"]],header=T,as.is=T,sep="\t",quote="\"")
 
 fftColumns <- 29:52 # 160-222
-selFreq <- #c("193","196","199")
+#selFreq <- #c("193","196","199")
 
 library(gplots)
 
 ##################################
   pdf(snakemake@output[["aveCor"]],width=8,height=15)
-  for (sample in samples)
-  {
-    fdata <- read.table(sprintf(snakemake@params[["WPSprefix"]],ID,sample),as.is=T,sep="\t",header=T,comment.char="~")
+  #for (sample in samples)
+  for (ID in IDs) {
+    sprintf(snakemake@params[["WPSprefix"]],ID,sample)
+    fdata <- read.table(sprintf(snakemake@params[["WPSprefix"]],ID),as.is=T,sep="\t",header=T,comment.char="~")
     colnames(fdata) <- sub("X","",colnames(fdata))
     rownames(fdata) <- fdata[,1]
     fdata <- fdata[,c(1,rev(c(2:dim(fdata)[2])))]
@@ -43,7 +44,6 @@ library(gplots)
     res <- cor(rowMeans(fdata[,selFreq]),logndata2[,order(names(logndata2))],use="pairwise.complete.obs")
     res <- data.frame(category=tLabels$Category,description=tLabels$Type,tissue=colnames(res),correlation=as.numeric(res))
     textplot(res[order(res$correlation),])
-    title(sample)
+    title(ID)
   }
   dev.off()
-
